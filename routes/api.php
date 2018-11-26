2c317d74bf8eea975c39bbcf29816ae132c8f7fd
Ibrahim Mubarak <ibrahim@faimtech.in>
2018-11-26 13:06:28 +0530

    Fix Registration

--- routes/api.php
+++ routes/api.php
@@ -18,5 +18,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 
-Route::post("login", "API/UserController@login");
-Route::post("register", "API/UserController@register");
+Route::post("login", "API\UserController@login");
+Route::post("register", "API\UserController@register");
