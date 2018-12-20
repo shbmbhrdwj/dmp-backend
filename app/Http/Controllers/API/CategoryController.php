ba2ac1f4263e8478eca2a4a84e9f927093579f54
Ibrahim Mubarak <ibrahim@faimtech.in>
2018-12-20 19:06:44 +0530

    Revert categories field

--- app/Http/Controllers/API/CategoryController.php
+++ app/Http/Controllers/API/CategoryController.php
@@ -17,8 +17,7 @@ class CategoryController extends Controller
     public function index()
     {
         $categories = new Categories(Category::all());
-//        return ['status' => 0, "message" => "Success", "data" => $categories];
-        return Category::all();
+        return ['status' => 0, "message" => "Success", "data" => $categories];
     }
 
     /**
